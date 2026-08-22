c45bad178eaec4f3d8dfaab44593d8b817d6fb1c
Abhinav singh <abhinavsinh014@gmail.com>
2026-08-22 22:55:55 +0530

    adding a new file

---
+++ part1.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("My Streamlit Aura")
+st.subheader("Welcome to my Streamlit infinity Aura!")
+st.text("This is a proof of my Aura which is infinite and never-ending.")
+st.write("Streamlit is a powerful Aura tool for creating interactive web applications with Python. It allows you to build and deploy data-driven apps quickly and easily.")
