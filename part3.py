9d1c7bb66738ecaf388d7f0e2f9d8499361c624f
Abhinav singh <abhinavsinh014@gmail.com>
2026-08-23 18:20:41 +0530

    adding a new file

--- part3.py
+++ part3.py
@@ -6,8 +6,19 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.header("Villian's Aura")
-    st.image("C:\\Users\\abhin\\Downloads\\doom.webp", caption="Thanos",width=300)
-
+    st.image("C:\\Users\\abhin\\Downloads\\doom.webp", caption="Doom",width=300)
+    vote_1 = st.button("Dr Doom Aura")
 with col2:
     st.header("Hero's Aura")
     st.image("C:\\Users\\abhin\\OneDrive\\Pictures\\licensed-image.jpg", caption="John cena",width=300)
+    vote_2 = st.button("John Cena Aura")
+
+if vote_1:
+    st.write("You selected Dr Doom's Aura")
+elif vote_2:
+    st.write("You selected John Cena's Aura")
+
+name = st.sidebar.text_input("Enter your name")
+side = st.sidebar.selectbox("Choose your side:", ["Hero", "Villian"])
+
+ 
