210253ce1d331a419243be2d1ac6b34d9eff32fe
Abhinav singh <abhinavsinh014@gmail.com>
2026-08-23 15:00:27 +0530

    adding a new file

--- part2.py
+++ part2.py
@@ -4,5 +4,17 @@ st.title("Aura App")
 if st.button("Want Aura"):
     st.success("Your aura is being activated")
 
-add_masala = st.checkbox("Increase Aura")
+add_masala = st.checkbox("increase Aura")
+
+if add_masala:
+    st.write("Aura added to you")
+
+Aura_type = st.radio("Pick your aura base: ", ["infinite", "powerful", "unloseable"])
+st.write(f"Selected base {Aura_type}")
+flavour = st.selectbox("Choose holder: ", ["None","Undertaker", "Henry Cavil", "John Cena", "Abhinav Thakur", "Lionel Messi", "Cristiano Ronaldo", "Virat Kohli"])
+if flavour=="None":
+    pass
+else:
+    st.write(f"Great choice, You got {flavour}'s Aura!")
+
 
