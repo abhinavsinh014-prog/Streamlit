695d5b472ea6cd551b2fab7b32097e83f074f77b
Abhinav singh <abhinavsinh014@gmail.com>
2026-08-24 22:42:52 +0530

    adding a new file

---
+++ part4.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Aura Legends")
+
+file = st.file_uploader("Upload a CSV file", type=["csv"])
+
+if file:
+    df= pd.read_csv(file)
+    st.subheader("Uploaded Data")
+    st.dataframe(df)
